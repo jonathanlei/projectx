a3f8d09bede0572328572045335b1e40235ebf5d
Alan Tseng <atseng202@gmail.com>
2021-03-29 15:08:22 -0700

    Add list of onboarding questions with feedback

---
+++ frontend_mvp/src/signupQuestions.js
@@ -0,0 +1,42 @@
+const signupQuestions = [
+  {
+    tag: "travelplanningfeeling",
+    question: "How do you feel about travel planning?",
+    options: [
+      [
+        "hate it",
+        "We’ve got you! Your travel curator will tailor travel plans for you, so you can focus on other things.",
+      ],
+      [
+        "it's okay",
+        "Gotcha. Having your own travel curator to tailor your travel plans and make your itinerary planning effortless. ",
+      ],
+      [
+        "love it",
+        "Great! So do we. Think of us as your new travel planning buddy.",
+      ],
+    ],
+  },
+  {
+    tag: "time",
+    question: "How much time & effort do you put into travel planning? ",
+    options: [
+      ["not much", "got it, we know that it can be tedious and time-consuming"],
+      ["some", "nice, we hear you. "],
+      ["a ton", "Sweet! We love it too"],
+    ],
+  },
+  {
+    tag: "frequency",
+    question: "How often do you plan on traveling after the pandemic?",
+    options: [
+      ["once a month or more", "we love an avid traveler!"],
+      [
+        "couple times a year",
+        "we get it, everyone needs to get away once in a while ",
+      ],
+      ["once a year", "Plan for the big yearly vacation!"],
+    ],
+  },
+];
+export default signupQuestions;
